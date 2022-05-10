a8ecb712e45971debb36962c846f6d78efbac545
Seraphym Haiduk <woodmangayduk@gmail.com>
2022-05-10 16:45:23 +0300

    build dirs removed

--- Plugins/ModelPlugin/build/CMakeFiles/ModelPlugin_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ModelPlugin_autogen"
-  "ModelPlugin_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ModelPlugin_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
